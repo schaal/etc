d0aea3837ed1b7e40a2fa70d3b454b2bff2e2e8e
Daniel Schaal <daniel@schaal.email>
2020-11-21 06:54:26 +0100

    Simplify zsh config, move to antibody

---
+++ zsh/init-zsh-config.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+set -e
+
+PATH_PREFIX="$0:P:h"
+
+export ANTIBODY_HOME="${PATH_PREFIX}/antibody_home"
+ANTIBODY="${ANTIBODY_HOME}/antibody"
+
+pushd "$PATH_PREFIX"
+
+trap 'popd' EXIT
+
+if [ ! -x "${ANTIBODY}" ]; then
+    curl -sfL https://git.io/antibody | sh -s - -b "$ANTIBODY_HOME"
+fi
+
+"${ANTIBODY}" bundle < zsh_plugins.txt > zsh_plugins.sh
+
+if ! grep -q "source \"${PATH_PREFIX}/my-zshrc\"" zshrc; then
+cat <<EOT >> ${PATH_PREFIX}/zshrc
+
+source "${PATH_PREFIX}/my-zshrc"
+EOT
+fi
+
