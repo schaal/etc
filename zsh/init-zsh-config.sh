01824a4218768a16a605820f43dc2e871d0a100d
Daniel Schaal <daniel@schaal.email>
2022-06-17 13:13:33 +0200

    Move to zinit

--- zsh/init-zsh-config.sh
+++ zsh/init-zsh-config.sh
@@ -4,22 +4,9 @@ set -e
 
 PATH_PREFIX="$0:P:h"
 
-export ANTIBODY_HOME="${PATH_PREFIX}/antibody_home"
-ANTIBODY="${ANTIBODY_HOME}/antibody"
-
 pushd "$PATH_PREFIX"
 
 trap 'popd' EXIT
-
-if [ ! -x "${ANTIBODY}" ]; then
-    curl -sfL https://git.io/antibody | sh -s - -b "$ANTIBODY_HOME"
-fi
-
-echo "Using $("${ANTIBODY}" home)"
-
-"${ANTIBODY}" bundle < zsh_plugins.txt > zsh_plugins.sh
-
-"${ANTIBODY}" update
 
 if ! grep -q "source \"${PATH_PREFIX}/my-zshrc\"" zshrc; then
 cat <<EOT >> ${PATH_PREFIX}/zshrc
