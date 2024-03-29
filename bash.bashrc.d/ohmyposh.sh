8490c6e843787d16b1c499991716f3e5d98c0263
Daniel Schaal <daniel@schaal.email>
2024-03-29 08:01:50 +0100

    Use oh-my-posh from etc

--- bash.bashrc.d/ohmyposh.sh
+++ bash.bashrc.d/ohmyposh.sh
@@ -1,2 +1,5 @@
-eval "$(oh-my-posh init bash --config etc/bash.bashrc.d/themes/${OHMYPOSH_THEME}.omp.json)"
+#OH_MY_POSH=$(dirname -- "$( readlink -f -- "$0"; )")/oh-my-posh
+OH_MY_POSH=/etc/bash.bashrc.d/oh-my-posh
+
+eval "$(${OH_MY_POSH} init bash --config /etc/bash.bashrc.d/themes/${OHMYPOSH_THEME}.omp.json)"
 
